1c509a9c9b5a930bc474a32096ca4063b445ec57
Protas <bryodiiidah@gmail.com>
2023-12-28 22:54:25 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,64 +1,6 @@
 import { useEffect, useState } from "react";
-import useScript from "../hooks/useScript";
 
 export default function App() {
-  const FEELPAY_CLIENT_ID = import.meta.env.VITE_APP_FEELPAY_CLIENT_ID;
-  const FEELPAY_CLIENT_SECRET = import.meta.env.VITE_APP_FEELPAY_CLIENT_SECRET;
-
-  const [amount, setAmount] = useState(1);
-
-  const { loading, error } = useScript(
-    "https://feelpay.vercel.app/packages/v1"
-  );
-
-  useEffect(() => {
-    const orderDetails = {
-      element: "dreamfeel-pay-button",
-      clientId: "e1c40a3f019f9d50",
-      clientSecret: "45c9f56f0464e0b78e4e1918b3df520e",
-      description: "",
-      order: {
-        // Default for one time order checkout.
-        installments: 1,
-        orderCompleteAfterInstallment: 1,
-        vat: 16, // percentage
-        amount: 3000,
-        currency: "KES", //Only KES supported for now
-        // Specify an array of order items.
-        items: [
-          {
-            id: "1",
-            name: "",
-            price: 0,
-            vat: 0,
-            url: ``,
-            image: "",
-          },
-        ],
-      },
-      onSuccess: (detail) => {
-        // Handle success
-        // const {feelpayCheckoutRequestID, feelpayCheckoutStatus, feelpayOrderId, ...} = detail
-        console.log(detail);
-      },
-      onError: (err) => {
-        // Handle error
-        // {message:""}
-        console.log(err);
-      },
-      // Run an action when innitialized!
-      onInit: () => {},
-      // Action when user cancels transaction!
-      onUserCancel: () => {},
-    };
-    setTimeout(() => {
-      const feelpay = new FeelPayWidget(orderDetails);
-      feelpay.init().then((pay) => {});
-    }, 1000);
-  });
-
-  if (loading) return <div>Loading</div>;
-  if (error) return <div>Error</div>;
   return (
     <div className="lg:px-48 min-h-screen dark:bg-gray-900">
       <div className="text-center">
@@ -111,7 +53,11 @@ export default function App() {
           />
         </div>
       </div>
-      <div id="dreamfeel-pay-button"></div>
+      <feelpay-launcher
+        clientSecret="0a7013c0fac16ea352b45cd5b52534bd"
+        clientKey="e07fea12a51701c1"
+        order="{}"
+      />
     </div>
   );
 }
